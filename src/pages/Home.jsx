de211bc1e53a2fe00bf33dea1551fb7a43ab8421
h1x0ver <huseynmg@code.edu.az>
2024-02-05 15:43:34 +0400

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Farid yatmayibdi Mehemmed ise ev tapsirigin etmeyibdir....
+        </div>
+    );
+};
+
+export default Home;
